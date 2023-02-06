10c2aa6b4d7684524575ebf09fc6013cee3ea322
Jarek Odzga <jarek.odzga@databricks.com>
2023-02-05 16:15:43 -0800

    Disable build publish to avoid checking for updates.

--- update.js
+++ update.js
@@ -2,11 +2,6 @@ const fs = require('fs');
 
 var packagejson = require('./lens/packages/open-lens/package.json');
 
-packagejson.build.publish = [{
-    url: "https://github.com/jodzga/OpenLens/releases/download/Latest",
-    provider: "generic"
-}];
-
 packagejson.build.win.artifactName = "OpenLens.Setup.${version}.${ext}";
 
 if (process.platform != "win32") {
